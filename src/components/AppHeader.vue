35a9ce399e48d3b121a9e048dda6178768f0e107
nixdie <nixdie@yandex.ru>
2020-06-07 15:54:17 +0700

    inital oath request

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="ui secondary pointing menu">
     <router-link to="/" class="active item">Image Storage</router-link>
+    <div class="right menu">
+      <a href="#" class="ui item" @click="login">Login 🍕</a>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+
 export default {
-  name: 'AppHeader'
+  name: 'AppHeader',
+  methods: {
+    ...mapActions(['login'])
+  }
 }
 </script>
 
